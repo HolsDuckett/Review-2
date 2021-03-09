badc56b49c2bc2d4f31c3be8b68cb19998f6c21d
HolsDuckett <hf.duckett@gmail.com>
2021-03-09 09:30:30 +0000

    freq below the minimum freq are modified and new sound wave returned

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -1,4 +1,16 @@
 
 def band_pass_filter(sound_wave, min_frequency, max_frequency)
-  sound_wave
+
+modified_sound_wave = []
+  sound_wave.each do | freq |
+    if freq < min_frequency
+      modified_sound_wave << min_frequency
+    else
+      modified_sound_wave << freq
+    end
+  end
+  return modified_sound_wave
 end
+
+
+# [10, 40, 110, 1000], 40, 1000    =>    [40, 40, 110, 1000] - 1 change up to min frequency
--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -6,7 +6,11 @@ describe 'band pass filter' do
     expect(band_pass_filter([40, 40, 110, 1000], 40, 1000)).to eq([40, 40, 110, 1000])
   end
 
+  it "should change any frequency below the minimum frequency and return modified soundwave" do
+    expect(band_pass_filter([10, 40, 110, 1000], 40, 1000)).to eq([40, 40, 110, 1000])
+  end
+
 
 end
 
-# [40, 40, 110, 1000], 40, 1000    =>    [40, 40, 110, 1000] - no change to frequency
+# [10, 40, 110, 1000], 40, 1000    =>    [40, 40, 110, 1000] - 1 change up to min frequency
